2464882067567407d13a35981dbde6203954280a
jsl <jesulbarreto@gmail.com>
2025-11-09 14:07:37 +0000

    Update keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -6,6 +6,7 @@ const server = http.createServer((req, res) => {
 server.listen(5000, "0.0.0.0", () => {
   //console.log(chalk.cyan("🌐 Keep-alive server running on http://0.0.0.0:5000"));
   //console.log(chalk.cyan("📍 Use this URL for Uptime Robot monitoring"));
+  return 0;
 });
 
 module.exports = server;
