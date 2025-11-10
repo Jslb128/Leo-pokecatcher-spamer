c8608c4a70687c81e5da907e2c2be7200642fd20
jsl <jesulbarreto@gmail.com>
2025-11-10 17:08:14 +0000

    Update catch.js

--- events/catch.js
+++ events/catch.js
@@ -139,6 +139,7 @@ module.exports = (client) => {
     }
 
     if (client.captchaPaused) {
+		message.channel.send("<@841738091227643914> captcha");
       console.log(
         `[${date.format(new Date(), "YYYY-MM-DD HH:mm:ss")}] Catch paused due to captcha for ${client.user.username}`,
       );
