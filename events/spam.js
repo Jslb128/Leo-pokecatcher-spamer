7d30f317c079d6ef546673f737f92e186d40cd5f
jsl <jesulbarreto@gmail.com>
2025-11-10 19:14:05 +0000

    Update spam.js

--- events/spam.js
+++ events/spam.js
@@ -3,11 +3,11 @@ const cron = require('node-cron');
 module.exports = (client) => {
   client.on("ready", async() => {
 
-		const channelId = '1436115271076024480'; // your channel ID here
-	        const channeli = await client.channels.fetch(channelId);
-	  cron.schedule('*/2 * * * * *', () => {
-		channeli.send("pick toothpick beugette bugatti");
-
-  	});
+	const channelId = '1436115271076024480'; // your channel ID here
+	const channel = await client.channels.fetch(channelId);
+	 while (true){
+		await sleep(1500);
+		channel.send("pick toothpick beugette");
+  	};
   });
 };
