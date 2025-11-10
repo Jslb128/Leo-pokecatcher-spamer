0747e3d1bbba65198a6ba4c11badaadab48d88ec
jsl <jesulbarreto@gmail.com>
2025-11-10 19:26:26 +0000

    Update spam.js

--- events/spam.js
+++ events/spam.js
@@ -5,13 +5,12 @@ function sleep(timeInMs) {
   });
 }
 module.exports = (client) => {
-  client.on("ready", async() => {
-
-	const channelId = '1436115271076024480'; // your channel ID here
-	const channel = await client.channels.fetch(channelId);
-	 while (true){
-		await sleep(1500);
-		channel.send("pick toothpick beugette");
-  	};
-  });
+	client.on("ready", async() => {
+		const channelId = '1436115271076024480'; // your channel ID here
+		const channel = await client.channels.fetch(channelId);
+		while (!client.captchaPaused){
+			await sleep(1500);
+			channel.send("pick toothpick beugette");
+		};
+	});
 };
