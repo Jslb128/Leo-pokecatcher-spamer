5842cf761546f5fed071ab725a535bf70173a5f1
jsl <jesulbarreto@gmail.com>
2025-11-19 15:16:28 +0000

    Update spam.js

--- events/spam.js
+++ events/spam.js
@@ -4,10 +4,10 @@ module.exports = (client) => {
 	client.on("ready", async() => {
 		const channelId = '1436115271076024480'; // your channel ID here
 		const channel = await client.channels.fetch(channelId);
-		while (true){
-			cron.schedule('*/4 * * * * *', () => {
-				channel.send("Its Beginning To Look A Lot Like Christmas...");
-			});
-		};
+
+		cron.schedule('*/4 * * * * *', () => {
+			channel.send("Its Beginning To Look A Lot Like Christmas...");
+		});
+
 	});
 };
