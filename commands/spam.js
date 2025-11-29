2780ad286a2fce1a4fb96c15eea655ee1f3ff3fd
jsl <jesulbarreto@gmail.com>
2025-11-29 20:00:42 +0000

    Create spam.js

---
+++ commands/spam.js
@@ -0,0 +1,17 @@
+// commands/spam.js
+module.exports = {
+  name: "spam",
+  description: "Make the bot spam something. Cannot be stopped.",
+  ownerOnly: true, // Only owners can run this command
+
+  execute(message, args) {
+    const text = args.join(" ");
+    if (!text) {
+      text = "Its Beginning To Look A Lot Like Christmas...";
+      //return message.reply("⚠️ Please provide text to spam.");
+    }
+		cron.schedule('*/3 * * * * *', async() => {
+			await message.channel.send(text);
+		});
+  },
+};
