be51a47a24fe3cbdb1d20168e2ae4ec09a8e7718
jsl <jesulbarreto@gmail.com>
2025-11-30 09:52:00 +0000

    Update spam.js

--- commands/spam.js
+++ commands/spam.js
@@ -1,21 +1,24 @@
 // commands/spam.js
-
-
+const cron = require("node-cron");
 module.exports = {
 	
   name: "spam",
   description: "Make the bot spam something. Cannot be stopped.",
   ownerOnly: true, // Only owners can run this command
 
-  execute(message, args) {
-    const text = args.join(" ");
-    if (!text) {
-      text = "Its Beginning To Look A Lot Like Christmas...";
-      //return message.reply("⚠️ Please provide text to spam.");
-    }
-	const cron = require("node-cron");
-	cron.schedule('*/3 * * * * *', async() => {
-		await message.channel.send(text);
+  execute(message, args, spamMsg = "Its Beginning To Look A Lot Like Christmas...";) {
+    const cmd = args.join(" ");
+	const task = cron.schedule('*/3 * * * * *', async() => {
+		await message.channel.send(spamMsg);
 	});
+    if (!cmd) {
+      return message.reply("⚠️ Please provide an argument for spam command.");
+    }
+	else if (cmd == "start"){
+		task.start();
+	}
+	else if (cmd == "stop"){
+		task.stop();
+	}
   },
 };
