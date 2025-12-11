3a49960ab665648dab768bb9262a4789c3dc09a1
jsl <jesulbarreto@gmail.com>
2025-12-11 17:14:26 +0000

    Update spam.js

--- events/spam.js
+++ events/spam.js
@@ -6,35 +6,37 @@ module.exports = (client) => {
   const chnnlName = "spam";
 
   client.on("ready", async () => {
-    
-    for (const [guildId, guild] of client.guilds.cache) {
-      try {
-        const channel = guild.channels.cache.find(
-          ch => ch.name === chnnlName && ch.type === "GUILD_TEXT"
-        );
-
-        if (!channel) {
-          console.log(`⚠️ No #${chnnlName} channel found in ${guild.name}`);
-          continue;
-        }
-
-        if (tasks.has(guildId)) continue;
-
-        const task = cron.schedule("*/3 * * * * *", async () => {
-          try {
-            await channel.send(spamMsg);
-          } catch (err) {
-            console.error(`❌ Spam failed in ${guild.name}:`, err.message);
+    if (client.captchaPaused = true){
+      for (const [guildId, guild] of client.guilds.cache) {
+        try {
+          const channel = guild.channels.cache.find(
+            ch => ch.name === chnnlName && ch.type === "GUILD_TEXT"
+          );
+  
+          if (!channel) {
+            console.log(`⚠️ No #${chnnlName} channel found in ${guild.name}`);
+            continue;
           }
-        });
-
-        tasks.set(guildId, task);
-        console.log(`✅ Auto-spamming started in ${guild.name} → #${channel.name}`);
-
-      } catch (err) {
-        console.error(`❌ Failed to start spam in ${guild.name}:`, err.message);
+  
+          if (tasks.has(guildId)) continue;
+  
+          const task = cron.schedule("*/3 * * * * *", async () => {
+            try {
+              await channel.send(spamMsg);
+            } catch (err) {
+              console.error(`❌ Spam failed in ${guild.name}:`, err.message);
+            }
+          });
+  
+          tasks.set(guildId, task);
+          console.log(`✅ Auto-spamming started in ${guild.name} → #${channel.name}`);
+  
+        } catch (err) {
+          console.error(`❌ Failed to start spam in ${guild.name}:`, err.message);
+        }
       }
     }
   });
+
 };
 
